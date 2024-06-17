b8ec7715cda4ed439a4550564aeb0124e38d9fca
Meghana <megharocks6886@gmail.com>
2024-06-17 20:01:00 +0530

    First commit

---
+++ src/test/java/demo_WD/demo_WD/Test1.java
@@ -0,0 +1,30 @@
+package demo_WD.demo_WD;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1
+
+{
+	@Test
+	public void amazonlogin() throws InterruptedException  {
+		
+		//   WebDriverManager.chromedriver().setup();
+		   WebDriverManager.chromedriver().clearDriverCache().setup();
+		   WebDriverManager.chromedriver().clearResolutionCache().setup();
+		   WebDriver driver=new ChromeDriver();
+		   driver.manage().window().maximize();
+		   driver.get("https://www.amazon.in");
+		   Thread.sleep(1000);
+		   driver.findElement(By.xpath("//div[@class='nav-line-1-container']")).click();
+		   Thread.sleep(2000);
+
+}
+}
